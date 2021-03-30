1a8a2daefee5d50468b4826c13e87ba0ae35cd08
Noah Bresler <noahbresler2002@gmail.com>
2021-03-30 05:28:41 -0500

    Enable webpack 5. Move head to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 // import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 
+import Head from "next/head";
 import type { AppProps } from "next/app";
+
 import { AuthProvider } from "@/firebase/auth";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <Head>
+        <title>BeeBeeLearn</title>
+        <link rel="icon" href="/bee.svg" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,17 @@
-import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-0 flex flex-col items-center justify-center bg-yellow-200">
-      <Head>
-        <title>BeeBeeLearn</title>
-        <link rel="icon" href="/bee.svg" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full min-h-screen">
-        <div className="flex flex-row items-center">
-          <h1 className="text-5xl font-extrabold tracking-wide">BeeBeeLearn</h1>
-          <img src="/bee-outline.svg" alt="BeeBee" className="ml-3 w-16 h-16" />
-        </div>
-        <div>
-          <Link href="/student">
-            <a>Student Home</a>
-          </Link>
-        </div>
-      </main>
-    </div>
+    <main className="w-full min-h-screen p-0 flex flex-col items-center justify-center bg-yellow-200 ">
+      <div className="flex flex-row items-center">
+        <h1 className="text-5xl font-extrabold tracking-wide">BeeBeeLearn</h1>
+        <img src="/bee-outline.svg" alt="BeeBee" className="ml-3 w-16 h-16" />
+      </div>
+      <div>
+        <Link href="/student">
+          <a>Student Home</a>
+        </Link>
+      </div>
+    </main>
   );
 }
