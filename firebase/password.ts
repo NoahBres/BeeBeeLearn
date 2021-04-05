e16dfde0d35b451d2879211b2cf5317a07baa5c8
Noah Bresler <noahbresler2002@gmail.com>
2021-04-05 03:56:37 -0500

    Switch to bcryptjs

--- firebase/password.ts
+++ firebase/password.ts
@@ -1,4 +1,4 @@
-import { genSalt, hash, compare } from "bcrypt";
+import { genSalt, hash, compare } from "bcryptjs";
 
 const SALT_ROUNDS = 12;
 
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { queryUser } from "@/firebase/query";
-import { comparePass } from "./password";
+import { comparePass } from "@/firebase/password";
 import { firebaseAdmin } from "@/firebase/firebaseAdmin";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
