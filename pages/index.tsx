34c1b4914bf1176a872e00498974098b7b674b92
Noah Bresler <noahbresler2002@gmail.com>
2021-04-16 13:50:51 -0500

    OpenGraph test for Addy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,26 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center w-full min-h-screen p-0 bg-yellow-200 ">
-      <div className="flex flex-row items-center">
-        <h1 className="text-5xl font-extrabold tracking-wide">BeeBeeLearn</h1>
-        <img src="/bee-outline.svg" alt="BeeBee" className="w-16 h-16 ml-3" />
-      </div>
-      <div>
-        <Link href="/student">
-          <a>Student Home</a>
-        </Link>
-      </div>
-    </main>
+    <>
+      <Head>
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1568526381923-caf3fd520382?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80"
+        />
+      </Head>
+      <main className="flex flex-col items-center justify-center w-full min-h-screen p-0 bg-yellow-200 ">
+        <div className="flex flex-row items-center">
+          <h1 className="text-5xl font-extrabold tracking-wide">BeeBeeLearn</h1>
+          <img src="/bee-outline.svg" alt="BeeBee" className="w-16 h-16 ml-3" />
+        </div>
+        <div>
+          <Link href="/student">
+            <a>Student Home</a>
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
